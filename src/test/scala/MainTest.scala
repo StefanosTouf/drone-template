cac5ab79a644bba46c57ff3ead7972abe1004e9c
StefanosTouf <stefetoufe@gmail.com>
2022-07-16 17:01:14 +0300

    fix failing tests

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -3,6 +3,6 @@ import org.scalatest.funsuite.{AnyFunSuite, AsyncFunSuite}
 
 class MainTest extends AnyFunSuite {
   test("Fibonacci of 10 is 55") {
-    assert(fibonacci(10) == 5)
+    assert(fibonacci(10) == 55)
   }
 }
